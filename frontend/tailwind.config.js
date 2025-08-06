a0883cd660890d9f46331036f22b258b72fdc273
HelpUSA <helpus.ecommerce@gmail.com>
2025-08-06 19:32:12 -0300

    Atualiza├º├úo autom├ítica

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,9 @@
+// tailwind.config.js
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
   theme: {
     extend: {},
   },
